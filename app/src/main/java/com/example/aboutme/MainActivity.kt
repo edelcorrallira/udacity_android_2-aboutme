38329a71787b42f4fb415d901952733c242b30fd
Eduardo <acorral@arkusnexus.com>
2019-07-25 22:23:20 -0700

    Implemented nickname button code

--- app/src/main/java/com/example/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -1,12 +1,34 @@
 package com.example.aboutme
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<Button>(R.id.done_button).setOnClickListener {
+            addNickname(it)// The done button, which was passed as an argument
+        }
+    }
+    private fun addNickname(view: View) {
+        val editText = findViewById<EditText>(R.id.nickname_edit)
+        val nickNameTextView = findViewById<TextView>(R.id.nickname_text)
+
+        nickNameTextView.text = editText.text
+        editText.visibility = View.GONE
+        view.visibility = View.GONE
+        nickNameTextView.visibility = View.VISIBLE
+
+        //Hide Keyboard
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken,0)
     }
 }
